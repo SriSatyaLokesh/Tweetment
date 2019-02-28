3fb627769a1c432acef6fc99fd73bb69e1990a06
siddhardha kommineni <36479482+siddu22k@users.noreply.github.com>
2019-02-28 22:11:35 +0530

    Changes made in the core code

--- tweetment.py
+++ tweetment.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request,send_from_directory
+from flask import Flask,render_template,request
 import os
 import sys,tweepy,csv,re
 from textblob import TextBlob
@@ -7,7 +7,7 @@ import speech_recognition as sr
 import pyaudio
 
 
-app = Flask(__name__,static_folder='static')
+app = Flask(__name__)
 #app.config.from_object(os.environ['APP_SETTINGS'])
 
 
@@ -24,7 +24,6 @@ def tweetment(hashtag,tweet_count):
     auth.set_access_token(accessToken, accessTokenSecret)
     api = tweepy.API(auth)
 
- 
     #temp=input("Press 0/v/V for voice input for Text input:")
             
     if  1 < 0 :
@@ -52,51 +51,67 @@ def tweetment(hashtag,tweet_count):
     else:       
         searchTerm = str(hashtag)
         NoOfTerms = int(tweet_count)
+        print(searchTerm)
+        print(NoOfTerms)
+        
 
     print("ANALYSING...............................................................!!!!!!")
 
     tweets = tweepy.Cursor(api.search, q=searchTerm, lang = "en").items(NoOfTerms)
+    print(searchTerm)
+    print(NoOfTerms)
+    #print(len([tweets]))
 
     polarity = 0
     positive = 0
-    wpositive = 0
+    #wpositive = 0
     spositive = 0
     negative = 0
-    wnegative = 0
+    #wnegative = 0
     snegative = 0
     neutral = 0
 
-
+    count1=0
     for tweet in tweets:
+        count1+=1
+        print("count-------------------------------------------------------->",count1)
         #Append to temp so that we can store in csv later. I use encode UTF-8
         #tweetText.append(cleanTweet(tweet.text).encode('utf-8'))
         # print (tweet.text.translate(non_bmp_map))    #print tweet's text
         analysis = TextBlob(tweet.text)
         polarity += analysis.sentiment.polarity
+        #print(analysis.sentiment.polarity)
         
         
         if (analysis.sentiment.polarity == 0):  # adding reaction of how people are reacting to find average later
             neutral += 1
-        elif (analysis.sentiment.polarity > 0.0 and analysis.sentiment.polarity <= 0.6):
+            print(analysis.sentiment.polarity,"neutral")
+        #elif (analysis.sentiment.polarity > 0 and analysis.sentiment.polarity <= 0.3):
+            #wpositive += 1
+        elif (analysis.sentiment.polarity > 0 and analysis.sentiment.polarity <= 0.6):
             positive += 1
-        elif (analysis.sentiment.polarity > 0.6 and analysis.sentiment.polarity <= 1.0):
-            spositive += 1 
-        elif (analysis.sentiment.polarity >= -0.6 and analysis.sentiment.polarity < 0.0):
+            print(analysis.sentiment.polarity,"positive")
+        elif (analysis.sentiment.polarity > 0.6 and analysis.sentiment.polarity <= 1):
+            spositive += 1
+            print(analysis.sentiment.polarity,"spositive")
+        #elif (analysis.sentiment.polarity > -0.3 and analysis.sentiment.polarity <= 0):
+            #wnegative += 1
+        elif (analysis.sentiment.polarity > -0.6 and analysis.sentiment.polarity < 0):
             negative += 1
-        elif (analysis.sentiment.polarity >= -1.0 and analysis.sentiment.polarity < -0.6):
+            print(analysis.sentiment.polarity,"negative")
+        elif (analysis.sentiment.polarity >= -1 and analysis.sentiment.polarity <= -0.6):
             snegative += 1
+            print(analysis.sentiment.polarity,"snegative")
+
             
 
     positive = percentage(positive, NoOfTerms)
-    print(positive)
+    #wpositive = percentage(wpositive, NoOfTerms)
     spositive = percentage(spositive, NoOfTerms)
-    print(spositive)
     negative = percentage(negative, NoOfTerms)
-    print(negative)
+    #wnegative = percentage(wnegative, NoOfTerms)
     snegative = percentage(snegative, NoOfTerms)
-    print(snegative)
     neutral = percentage(neutral, NoOfTerms)
-    print(neutral)
 
     # finding average reaction
     polarity = polarity / NoOfTerms
@@ -104,17 +119,17 @@ def tweetment(hashtag,tweet_count):
 
     if (polarity == 0):
         gp="Neutral"
-    elif (polarity > 0.0 and polarity <= 0.3):
-        gp="Weakly Positive"
-    elif (polarity > 0.3 and polarity <= 0.6):
+    #elif (polarity > 0 and polarity <= 0.3):
+        #gp="Weakly Positive"
+    elif (polarity > 0 and polarity <= 0.6):
         gp="Positive"
-    elif (polarity > 0.6 and polarity <= 1.0):
+    elif (polarity > 0.6 and polarity <= 1):
        gp="Strongly Positive"
-    elif (polarity > -0.3 and polarity <= 0.0):
-       gp="Weakly Negative"
-    elif (polarity > -0.6 and polarity <= -0.3):
+    #elif (polarity > -0.3 and polarity <= 0):
+       #gp="Weakly Negative"
+    elif (polarity > -0.6 and polarity < 0):
        gp="Negative"
-    elif (polarity > -1.0 and polarity <= -0.6):
+    elif (polarity >= -1 and polarity <= -0.6):
        gp="Strongly Negative"
         
         
@@ -123,7 +138,7 @@ def tweetment(hashtag,tweet_count):
 
     labels = ['Positive [' + str(positive) + '%]','Strongly Positive [' + str(spositive) + '%]', 'Strongly Negative [' + str(snegative) + '%]', 'Neutral [' + str(neutral) + '%]','Negative [' + str(negative) + '%]' ]
     sizes = [positive, spositive,snegative, neutral, negative ]
-    colors = ['#64dd17','#1b5e20','#B22222', '#ffff00', '#FF4500']
+    colors = ['#64dd17','#1b5e20','#d50000', '#ffff00', 'red']
 
     
     # only "explode" the 2nd slice (i.e. 'Hogs')
@@ -144,20 +159,20 @@ def tweetment(hashtag,tweet_count):
 
     # Equal aspect ratio ensures that pie is drawn as a circle
     plt.axis('equal')  
-    files= os.listdir('static/img/')
+    files= os.listdir('/home/sid/tweetment/Tweetment/static/img')
     if 'result.png' in files:
-        os.remove('static/img/result.png')
+        os.remove('/home/sid/tweetment/Tweetment/static/img/result.png')
     fig2 = plt.gcf()
-    fig2.savefig('static/img/result.png',bbox_inches="tight",dpi=400,transparent=True)
+    fig2.savefig('/home/sid/tweetment/Tweetment/static/img/result.png',bbox_inches="tight",dpi=400,transparent=True)
     return results
 
 #home() serves the home page(i.e index.html
 
 @app.route('/')
 def home():
-    files= os.listdir('static/img/')
+    files= os.listdir('/home/sid/tweetment/Tweetment/static/img/')
     if 'result.png' in files:
-        os.remove('static/img/result.png')
+        os.remove('/home/sid/tweetment/Tweetment/static/img/result.png')
     return render_template("index.html")
 
 
@@ -165,11 +180,6 @@ def home():
 '''@app.route('/<filename :path>')
 def send_static(filename):
     return static_file(filename, root = 'static/')'''
-
-@app.route('/<path:filename>')  
-def send_file(filename):  
-    return send_from_directory(app.static_folder, filename)
-
 
 @app.route("/result",methods = ['POST'])
 def analyize():
